8b797366f252c6a5deb0f3db56283ece05fe876d
Charlie Harrison <charlieisharrison@gmail.com>
2014-10-20 12:58:01 -0400

    update R -> no more striping

--- src/R.c
+++ src/R.c
@@ -1,79 +1,44 @@
 #include "R.h"
 /* R */
 
-// private methods
-pthread_rw_lock_t *_get_lock(r_t *r, addr_t addr);
-
-r_t *setup_r(addr_t size, int log_num_locks) {
+r_t *r_setup(addr_t size, int log_num_locks) {
 	r_t *r = (r_t *) malloc(sizeof(r_t));
 
 	// initialize as null
-	set_t *dests = (set_t *) malloc(size * sizeof(set_t*));
-	memset(dest, 0, size * sizeof(set_t*));
-
-	pthread_rw_lock_t *locks = (pthread_rw_lock_t *)malloc(r->num_locks * sizeof(pthread_rw_lock_t *));
+	skip_list_t **dests = (skip_list_t **) malloc(size * sizeof(skip_list_t *));
+	memset(dest, 0, size * sizeof(skip_list_t *));
 
 	r->dests = dests;
 	r->size = size;
-	r->num_locks = 1 << log_num_locks;
-	r->locks = locks;
 
-	if (r == NULL || dests == NULL || locks == NULL) {
+	if (r == NULL || dests == NULL) {
 		perror('malloc');
-	}
-
-	int i;
-	for (i = 0; i < r->num_locks; i++) {
-		if((pthread_rwlock_init(locks[i], NULL))) {
-			perror('pthread_rwlock_init');
-			exit(1);
-		}
 	}
 
 	return r;
 }
 
-void tear_down(r_t *r) {
+void r_tear_down(r_t *r) {
 	int i;
 	for (i = 0 ; i < r->size; i++) {
-		if (r->dests[i]) {
-			_set_tear_down(r->dests[i]);
+		if (r->dests[i] != NULL) {
+			skip_list_tear_down(r->dests[i]);
 		}
 	}
 	free(r->dests);
-	for(i = 0; i < r->num_locks; i++) {
-		pthread_rwlock_destroy(r->locks[i]);
-	}
-	free(r->locks);
 	free(r);
 }
 
-char accept(r_t *r, addr_t dest, addr_t src) {
-	set_t *set = r->dests[dest];
-	pthread_rw_lock_t *lock = _get_lock(r, dest);
-
-	pthread_rwlock_rdlock(lock);
-	int accept = set_accept(set, src);
-	pthread_rwlock_unlock(lock);
-
-	return accept;
+char r_accept(r_t *r, addr_t dest, addr_t src) {
+	skip_list_t *set = r->dests[dest];
+	return skip_list_contains(set, src);
 }
 
-void update(r_t *r, char to_add, addr_t dest, addr_t begin, addr_t end) {
-	set_t *set = r->dests[dest];
-	pthread_rw_lock_t *lock = _get_lock(r, dest);
-
-	pthread_rwlock_wrlock(lock);
+void r_update(r_t *r, char to_add, addr_t dest, addr_t begin, addr_t end) {
+	skip_list_t *set = r->dests[dest];
 	if (to_add) {
-		_set_add_range(set, begin, end);
+		skip_list_add_range(set, begin, end);
 	} else {
-		_set_remove_range(set, begin, end);
+		skip_list_remove_range(set, begin, end);
 	}
-	pthread_rwlock_unlock(lock);
-}
-
-pthread_rw_lock_t *_get_lock(r_t *r, addr_t addr) {
-	addr_t stripe_size = r->num_locks / r->size;
-	addr_t index = addr / stripe_size;
-	return r->locks[index];
 }
--- src/include/R.h
+++ src/include/R.h
@@ -1,26 +1,16 @@
-
-typdef addr_t uint16_t;
-
-/* set represented as a skip list of address ranges */
-typedef struct set {
-
-} set_t;
-
+#include "containers.h"
 
 /* R */
 
 typedef struct r {
 	addr_t size;
-	set_t *dests;
-	int num_locks;
-	pthread_rwlock_t *locks;
+	skip_list_t **sl;
 } r_t;
 
-r_t *setup_r(addr_t size, int log_num_locks);
+r_t *r_setup(addr_t size);
 
-void tear_down(r_t *r);
+void r_tear_down(r_t *r);
 
-char accept(r_t *r, addr_t dest, addr_t src);
+char r_accept(r_t *r, addr_t dest, addr_t src);
 
-void update(r_t *r, addr_t dest, addr_t begin, addr_t end);
-
+void r_update(r_t *r, char to_add, addr_t dest, addr_t begin, addr_t end);
