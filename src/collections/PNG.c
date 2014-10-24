d5072af4e2900be8f5a3ac2f54d78a78dbc6e940
Shanna Chen <sxchen@cs.brown.edu>
2014-10-24 13:30:06 -0400

    filled in getAndIncrement and compareAndSwap for PNG and histogram

--- src/collections/PNG.c
+++ src/collections/PNG.c
@@ -1,17 +1,17 @@
 #include "types.h"
+#include "stdatomic.h"
 /*PNG*/
 typedef struct png {
-    int dest[1<<MAX_ADDR] = {0};
+    atomic_int dest[1<<MAX_ADDR] = {0};
 
 } png_t;
 
 //get
 int png_get(png_t *png, int addr){
-        dest = p.dest;
-        return dest[addr];
+        return (int)atomic_load(png->dest[addr]);
 }
 
 //compareAndSwap
-void png_getAndIncrement(png_t *png, int addr){
-        
+bool png_getAndIncrement(png_t *png, int addr, atomic_int expected, atomic_int swap){
+        return atomic_compare_exchange_strong(png->dest[addr],expected,swap); 
 }
--- src/collections/histogram.c
+++ src/collections/histogram.c
@@ -1,17 +1,18 @@
 #include "types.h"
+#include "stdatomic.h"
 /*histogram*/
+
 typedef struct h {
-    int hist[1<<MAX_ADDR] = {0};
+    atomic_int hist[1<<MAX_ADDR] = {0};
 
 } h_t;
 
 //get
 int histogram_get(h_t *h, int addr){
-        hist = h.hist;
-        return hist[addr];
+        return (int)atomic_load(h->hist[addr]);
 }
 
 //getAndIncrement
 void histogram_getAndIncrement(h_t *h, int addr){
-        
+        atomic_fetch_add(h->hist[addr],1);     
 }
