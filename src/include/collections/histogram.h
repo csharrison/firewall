4efca94cef0a8729c9fbd6b170ee1bb4a963c739
Shanna Chen <sxchen@cs.brown.edu>
2014-10-26 15:48:13 -0400

    fixing histogram merge conflicts

--- src/include/collections/histogram.h
+++ src/include/collections/histogram.h
@@ -1,5 +1,4 @@
 #include "types.h"
-<<<<<<< HEAD
 #include "stdatomic.h"
 /*histogram*/
 
@@ -13,6 +12,3 @@ hist_t *hist_setup();
 void hist_tear_down(hist_t *hist);
 
 void hist_update(hist_t *hist, uint16_t fingerprint);
-=======
-void hist_add(hist_t *h, addr_t addr);
->>>>>>> 2c1e2a28886b6ceea0c0b9145325f62d98f8ea89
