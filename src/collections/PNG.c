89f3d67581394dcb14239839fde524d8e19a36b8
Shanna Chen <sxchen@cs.brown.edu>
2014-10-26 21:18:28 -0400

    used memset for PNG and histogram

--- src/collections/PNG.c
+++ src/collections/PNG.c
@@ -5,17 +5,12 @@
 
 png_t *png_setup() {
 	png_t *png = (png_t *) malloc(sizeof(png_t));
+	memset(png->dest,0,1<<MAX_ADDR);
 
-	// initialize as null
-        
-	atomic_char dest[1<<MAX_ADDR] = {'0'};
-
-        png->dest = dest;
-
-        if (png == NULL){
-                perror("malloc");
-        }
-
+	if (png == NULL){
+		perror("malloc");
+    }
+	
 	return png;
 }
 
@@ -24,11 +19,11 @@ void png_tear_down(png_t *png) {
 }
 
 int png_allow(png_t *png, addr_t addr) {
-        return atoi(atomic_load(png->dest[addr]));
+	return atoi(atomic_load(png->dest[addr]));
 }
 
 void png_update(png_t *png, addr_t addr, char allow){
-        atomic_char val = '0';
-        atomic_compare_exchange_strong(png->dest[addr], &val, allow);
+	atomic_char val = '0';
+	atomic_compare_exchange_strong(png->dest[addr], &val, allow);
 }
 
--- src/collections/histogram.c
+++ src/collections/histogram.c
@@ -3,26 +3,21 @@
 /*histogram*/
 
 hist_t *hist_setup(){
-        hist_t *hist = (hist_t *) malloc(sizeof(hist_t));
+	hist_t *hist = (hist_t *) malloc(sizeof(hist_t));
+    memset(hist->dest, 0, 1<<MAX_ADDR)
 
-	// initialize as null
-        
-	atomic_long dest[1<<MAX_ADDR] = {0};
-
-        hist->dest = dest;
-
-        if (hist== NULL){
-                perror("malloc");
-        }
+	if (hist== NULL){
+		perror("malloc");
+	}
 
 	return hist;
 
 }
 
 void hist_tear_down(hist_t *hist){
-        free(hist);
+	free(hist);
 }
 
 void hist_update(hist_t *hist, uint16_t fingerprint){
-        atomic_fetch_add(h->hist[fingerprint], 1);
+	atomic_fetch_add(h->hist[fingerprint], 1);
 }
